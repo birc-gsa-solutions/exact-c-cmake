6b96d1d86f1b81d61e8265103ffd5ad41b512ef0
Thomas Mailund <mailund@birc.au.dk>
2022-01-28 06:26:27 +0100

    fixed sam output

--- src/sam.c
+++ src/sam.c
@@ -5,5 +5,5 @@
 void print_sam(FILE *f, const char *rname, const char *read, const char *chrom, int pos)
 {
     int m = strlen(read);
-    fprintf(f, "%s\t%s\t%d\t%*c\t%s\n", rname, chrom, pos + 1, m, 'M', read);
+    fprintf(f, "%s\t%s\t%d\t%dM\t%s\n", rname, chrom, pos + 1, m, read);
 }
